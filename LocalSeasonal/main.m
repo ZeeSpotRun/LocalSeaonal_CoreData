6bdfeaf7de0e7d1675a888bc905a00901dd18f05
Zoe Malcolm <zoe.malcolm@gmail.com>
2014-07-27 22:07:42 -0400

    Initial Commit

---
+++ LocalSeasonal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LocalSeasonal
+//
+//  Created by Makeba Zoe Malcolm on 27/07/14.
+//  Copyright (c) 2014 Zoe Malcolm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
